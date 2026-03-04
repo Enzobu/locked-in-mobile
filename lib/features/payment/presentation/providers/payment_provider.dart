368cc8bdc19c7e54b2c18af5b443b3343da0b7a8
Yanis <yanis.marsi.34@gmail.com>
2026-03-04 16:54:36 +0100

    fix: TK-033 use MockPaymentService by default until backend Stripe is configured

--- lib/features/payment/presentation/providers/payment_provider.dart
+++ lib/features/payment/presentation/providers/payment_provider.dart
@@ -3,8 +3,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/network/dio_client.dart';
 import '../../data/datasources/api_payment_datasource.dart';
 import '../../data/datasources/payment_datasource.dart';
+import '../../data/services/mock_payment_service.dart';
 import '../../data/services/stripe_payment_service.dart';
 import '../../domain/services/payment_service.dart';
+
+/// Set to true to use the real Stripe payment flow.
+/// Requires the backend to have STRIPE_SECRET_KEY configured.
+const _useStripe = false;
 
 final paymentDatasourceProvider = Provider<PaymentDatasource>((ref) {
   final client = ref.watch(dioClientProvider);
@@ -12,6 +17,9 @@ final paymentDatasourceProvider = Provider<PaymentDatasource>((ref) {
 });
 
 final paymentServiceProvider = Provider<PaymentService>((ref) {
-  final datasource = ref.watch(paymentDatasourceProvider);
-  return StripePaymentService(datasource: datasource);
+  if (_useStripe) {
+    final datasource = ref.watch(paymentDatasourceProvider);
+    return StripePaymentService(datasource: datasource);
+  }
+  return MockPaymentService();
 });
